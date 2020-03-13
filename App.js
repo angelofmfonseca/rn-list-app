42ff90596f7cfe4bd1eeca8e84be6dfb0f48e488
Angelo Fonseca <angelofonseca@Mac-mini-de-Angelo.local>
2020-03-12 23:11:25 -0300

    layout da area do input

--- App.js
+++ App.js
@@ -3,26 +3,35 @@ import { StyleSheet, Text, View, Button, TextInput } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.general}>
-      <View>
-        <TextInput
-          placeholder="Course goal"
-          style={styles.enterGoal}
-        ></TextInput>
+    <View style={styles.screen}>
+      <Text style={styles.title}>Shop List</Text>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Enter an item" style={styles.input}></TextInput>
         <Button title="ADD" />
       </View>
-      <View></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  general: {
+  screen: {
     padding: 50
   },
-  enterGoal: {
+  title: {
+    fontSize: 22,
+    textAlign: "center",
+    marginTop: 10,
+    marginBottom: 30
+  },
+  inputContainer: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center"
+  },
+  input: {
     borderBottomWidth: 1,
-    borderBottomColor: "#000",
-    padding: 5
+    borderBottomColor: "#aaa",
+    padding: 5,
+    width: "80%"
   }
 });
